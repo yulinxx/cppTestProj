43b7d08bf7331889aef661b0f81b6a94c7071ea0
yulinxx <yulinxx@gmail.com>
2022-11-04 22:30:30 +0800

    代码调整

--- base/template.cpp
+++ base/template.cpp
@@ -49,10 +49,6 @@
  */
 
 #include <stdio.h>
-
-#define TESTX(x) \
-    std::cout << ##x##x #x #x << std::endl;
-
 #include <iostream>
 
 int nFlag = 0x00;
@@ -85,60 +81,8 @@ struct Objx<NumType, 2>
     }
 };
 
-class ObjPtr
-{
-public:
-    int n = 99;
-    int m = 88;
-};
 
-const double eps = 1e-8;
-typedef struct _InOutParam_
-{
-    // 基本参数 需设定
-    double m_dLen;   // 长度
-    double m_dAngle; // 角度
-
-    _InOutParam_()
-    {
-        m_dLen = 5;
-        m_dAngle = 10;
-    }
-
-    bool operator==(const _InOutParam_ &p) const
-    {
-        return fabs(this->m_dLen - p.m_dLen) < eps &&
-               fabs(this->m_dAngle - p.m_dAngle) < eps;
-    }
-
-    bool operator!=(const _InOutParam_ &p) const
-    {
-        if (this == &p)
-            return false;
-        else
-            return true;
-    }
-
-} InOutParam;
-
-class Base
-{
-public:
-    int a = 9;
-
-protected:
-    int b = 19;
-};
-
-class SubObj : Base
-{
-public:
-    int c = 99;
-};
-
-
-
-
+///////////////////////////////////
 struct testStruct
 {
     int n = 0;
@@ -147,13 +91,17 @@ struct testStruct
 
 void testFunc(testStruct* pS)
 {
-    std::cout << pS->n << "\t" << pS->d << std::endl;
+    std::cout << pS->n << "\t" << pS->d << std::endl;   // 0	0
 
+    // 指针地址             指针地址            指针地址的地址      指针地址
+    // 0000023A5C671190	0000023A5C671190	000000CAB76FFBC0	0000023A5C671190
     std::cout << pS << "\t" << (pS) << "\t" << &pS << "\t"
               << static_cast<void *>(pS) << std::endl;
 }
 
 
+///////////////////////////////////
+// char  string 转换
 void charStrSwitch()
 {
     unsigned char myString[] = "This is my string\n12345";  // 24
@@ -167,85 +115,10 @@ void charStrSwitch()
     std::cout << resData << std::endl;
 }
 
-int main()
+///////////////////////////////////
+// 位的与或非
+void bitTest()
 {
-    charStrSwitch();
-
-    // testStruct* p = new testStruct(3, 9.9);
-    testStruct* p = new testStruct();
-    testFunc(p);
-
-    // SubObj obj;
-    // auto tempA = obj.a;
-    // auto temp = obj.b;
-
-    int n = -17 % 8;
-
-    int nSize = 8;
-    int nMaxPtX = 6;
-    double dXPrev = (nMaxPtX - 2) < 0 ? nSize + (nMaxPtX - 2) : (nMaxPtX - 2);
-    double dYPrev = (nMaxPtX - 1) < 0 ? nSize + (nMaxPtX - 1) : (nMaxPtX - 1);
-    double dXNext = (nMaxPtX + 2) % nSize;
-    double dYNext = (nMaxPtX + 3) % nSize;
-
-    double dx1 = 10.0;
-    double dy1 = 0.0;
-
-    double dx2 = 5.0;
-    double dy2 = 0.0;
-
-    double dx3 = 5.0;
-    double dy3 = 5.0;
-
-    double dxv1 = dx2 - dx1;
-    double dyv1 = dy2 - dy1;
-
-    double dxv2 = dx3 - dx2;
-    double dyv2 = dy3 - dy2;
-
-    double dRes = dxv1 * dyv2 - dyv1 * dxv2;
-
-    InOutParam a;
-    a.m_dAngle = 10;
-
-    InOutParam b;
-    b.m_dAngle = 10.0001;
-
-    if (a == b)
-    {
-        std::cout << " =========== ";
-    }
-
-    if (a != b)
-    {
-        std::cout << "aaaaaaaaaaa";
-    }
-
-    std::shared_ptr<ObjPtr> pObj = nullptr;
-
-    if (!pObj)
-    {
-        // pObj = std::shared_ptr<ObjPtr>(new ObjPtr());
-        pObj = std::make_shared<ObjPtr>();
-    }
-
-    if (!pObj)
-    {
-        // pObj = std::shared_ptr<ObjPtr>(new ObjPtr());
-        pObj = std::make_shared<ObjPtr>();
-    }
-
-    int nTest = pObj->m;
-
-    Objx<int, 4> xa;
-    Objx<char, 2> xb;
-    return 0;
-
-    ObjA<int> objA;
-    ObjB<int, 5> objB;
-
-    TESTX("abcdef")
-
     int nFlag = 0x00;
     while (!(nFlag & 0x11))
     {
@@ -288,7 +161,6 @@ int main()
     nFlag = 0x00;
 
     {
-
         int c = nFlag & 0x10;
         nFlag |= 0x10;
         bool d = nFlag & 0x10;
@@ -299,6 +171,72 @@ int main()
 
         bool m = b;
     }
+}
+
+
+
+
+
+
+
+
+
+///////////////////////////////////
+int main()
+{
+    // This is my string
+    // 12345
+    // This is my string
+    // 12345
+    charStrSwitch();    
+
+    testStruct* p = new testStruct();
+    testFunc(p);
+
+    bitTest();
+
+    int n = -17 % 8;    // -1
+
+    // 向量叉乘 判断两条共点的线的方向
+    double dx1 = 10.0;
+    double dy1 = 0.0;
+
+    double dx2 = 5.0;
+    double dy2 = 0.0;
+
+    double dx3 = 5.0;
+    double dy3 = 5.0;
+
+    double dxv1 = dx2 - dx1;    // -5.0000000000000000
+    double dyv1 = dy2 - dy1;    // 0.0000000000000000
+
+    double dxv2 = dx3 - dx2;    // 0.0000000000000000
+    double dyv2 = dy3 - dy2;    // 5.0000000000000000
+
+    double dRes = dxv1 * dyv2 - dyv1 * dxv2;    // -25.000000000000000
+
+    Objx<int, 4> xa;
+    Objx<char, 2> xb;
+
+    ObjA<int> objA;
+    ObjB<int, 5> objB;
+
+    // testTemp();
+    
 
     return 0;
 }
+
+
+/*
+
+This is my string
+12345
+This is my string
+12345
+0       0
+00000183D8DC32D0        00000183D8DC32D0        0000004C2CF3F6F0        00000183D8DC32D0
+aaaaaaaaaaaobjx aaaaaaa  
+objx bbbbbbbb  
+
+*/
