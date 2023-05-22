ca6779e5187b6586fbecc0122543f42062eb7ac9
yulinxx <yulinxx@gmail.com>
2023-05-22 20:55:13 +0800

    新增：C++11 类型的判断--- 类、枚举，结构 体的判断

---
+++ cpp11/typeJudge.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <type_traits>
+
+union A { };
+struct B { };
+enum C { };
+class D { };
+ 
+template<typename T>
+void foo()
+{
+    std::cout << std::is_class<T>::value << std::endl;
+}
+ 
+int main()
+{
+    std::cout << "C is class ?: ";
+    foo<C>();
+    std::cout << "B is class ?: ";
+    foo<B>();
+    std::cout << "D is class ?: " << std::is_class<D>::value << std::endl;
+    std::cout << "A is union ?: " << std::is_union<A>::value << std::endl;
+    C c;
+    std::cout << "c is enum ?: " << std::is_enum<decltype(c)>::value << std::endl;
+    return 0;
+}
