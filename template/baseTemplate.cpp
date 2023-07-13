833cb985b285f4280647b303e6c59f814f96dc51
xx <xx@ubuntu.com>
2023-07-14 00:12:26 +0800

    新增 --- 模板 的一些使用场景

--- template/baseTemplate.cpp
+++ template/baseTemplate.cpp
@@ -14,6 +14,27 @@
 
 // C++的模板并非直接可用的代码，需要经历「实例化」，而实例化的过程其实就是指定参数的过程。
 
+// template <typename T>
+// void f(const T &t) {}
+// 对于那些基本类型（比如说int、char之类的）来说，是徒增了它的开销吗? 为什么
+
+// 对于基本类型（如 int、char 等），传递参数作为 const T& 的形式可能会增加一些开销。
+// 这是因为模板函数 f 会对传递的参数进行类型推断，并使用对应的类型 T 进行实例化。
+// 对于基本类型，包括内置类型，这意味着会进行额外的参数拷贝或引用。
+
+// 当使用 const T& 进行参数传递时，会发生以下过程：
+
+// 1. 对于非引用类型的参数，会创建一个临时对象来进行拷贝构造，以便传递给模板函数。
+// 2. 对于引用类型的参数，不会发生额外的拷贝构造，但会增加对临时对象的引用。
+
+// 因此，对于基本类型，可能会增加一些开销，例如拷贝构造函数的调用或引用计数的增加。
+// 这个开销通常是微不足道的，对于大多数情况下的基本类型，不会对性能产生显著影响。
+
+// 然而，对于某些特殊情况，比如需要频繁调用的高性能代码段，可以考虑使用非模板函数或使用传值方式（const T）进行参数传递，以避免不必要的开销。
+
+// 总结起来，对于基本类型，使用模板函数和传递 const T& 形式的参数可能会引入一些微小的开销，但在大多数情况下这种开销是可以忽略的。
+// 如果性能是关键考虑因素，可以选择其他方式来处理基本类型参数
+
 ////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename NumType>
 class Obj
@@ -92,7 +113,6 @@ void Demo()
   TestB<arr2[1]> t7; // 常量表达式修饰的普通数组成员OK
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // 「模板类型的模板参数」，其实就是嵌套模板的意思，把「某一种类型的模板」作为一个参数传给另一个模板
 // 模板类型的模板参数
@@ -102,7 +122,6 @@ void f()
   Tem<int, std::string> te;
   te.show();
 }
-
 
 // 符合条件的模板类
 template <typename T1, typename T2>
@@ -133,17 +152,105 @@ void Demo2()
   // f<Test3>(); // ERR，类型不匹配
 }
 
+namespace Simple
+{
+  // 一个简单的函数模板例子
+  template <typename T>
+  T get_max(T a, T b)
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    std::cout << a << " " << b << std::endl;
+    auto result = a > b ? a : b;
+    return result;
+  }
 
+  // 提供特化版本，处理某些特定类型
+  template <>
+  float get_max(float a, float b)
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    std::cout << "float : " << a << " " << b << std::endl;
+    auto result = a > b ? a : b;
+    return result;
+  }
+
+  // 提供偏特化版本，解决传递指针的问题
+  template <typename T>
+  T get_max(T *a, T *b)
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    std::cout << *a << " " << *b << std::endl;
+    auto result = *a > *b ? *a : *b;
+    return result;
+  }
+
+  // 默认值模板
+  template <typename T = std::string>
+  T get_max(std::string a, std::string b) // 默认值这里的参数类型需要直接指定
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    std::cout << "STRING : " << a << " " << b << std::endl;
+    return a;
+  }
+
+  // 尾置返回类型
+  template <typename T1, typename T2>
+  auto get_max(T1 a, T2 b) -> decltype(b < a ? a : b)
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    std::cout << a << " " << b << std::endl;
+    return a > b ? a : b;
+  }
+
+  void Demo()
+  {
+    int a = 8, b = 3;
+    auto c = a + b;    // 运行时需要实际执行a+b，哪怕编译时就能推导出类型
+    decltype(a + b) d; // 编译期类型推导
+    // auto c;  // error // 不可以用auto c; 直接声明变量，必须同时初始化。
+  }
+}
+
+namespace Decay
+{
+  // decay   英 [dɪˈkeɪ]美 [dɪˈkeɪ]
+  // v. （建筑、地方等）破败，衰落；（观念、影响力等）衰败；（使）腐朽，腐烂
+  // 模板匹配decay机制
+  // 模板在进行匹配的时候，会进行一个退化，比如const int 如果找不到对应的类型，会退化为int。
+  // 其实就是把各种引用等修饰去掉，把cosnt int&退化为int。
+
+  // 模板类型退化 decay
+  template <typename T>
+  T get_max_decay(T a, T b)
+  {
+    std::cout << "FUNCTION NAME : " << __FUNCTION__ << ", LINE : " << __LINE__ << std::endl;
+    return a > b ? a : b;
+  }
+
+  void Demo()
+  {
+    // 模板类型退化 decay
+    // 看着比较抽象，其实就是把各种引用啊什么的修饰去掉，把cosnt int&退化为int
+    int const c = 42;
+    int i = 1;
+    get_max_decay(i, c); // OK: T 被推断为 int，c 中的 const 被 decay 掉
+    get_max_decay(c, c); // OK: T 被推断为 int
+
+    int &ir = i;
+    get_max_decay(i, ir); // OK: T 被推断为 int， ir 中的引用被 decay 掉
+
+    // ...
+  }
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-class  inlFileTestClass
+class inlFileTestClass
 {
 public:
-    void foo();
+  void foo();
 };
 
 #include "baseTemplate.inl" // 包含 .inl 文件
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
@@ -159,8 +266,6 @@ int main()
   Array<int, 5> arr; // 实例化，并创建对象
   arr.at(1) = 6;
 
-
-
   // 变参模板 的使用
   auto a = sum(1, 2, 3, 4);                       // 10
   auto b = sum(1.5, 2, 3l);                       // 6.5
@@ -173,5 +278,7 @@ int main()
   inlFileTestClass inlObj;
   inlObj.foo();
 
+  Simple::Demo();
+  Decay::Demo();
   return 0;
 }
