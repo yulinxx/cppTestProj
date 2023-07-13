e8cf845d6a99983108acfd676a378db2af4acc34
yulinxx <yulinxx@gmail.com>
2023-07-13 18:05:08 +0800

    修正 -- 编译错误

--- boost/src/geometry/pt2pt.cpp
+++ boost/src/geometry/pt2pt.cpp
@@ -9,6 +9,23 @@
 
 namespace bg = boost::geometry;
 
+// 自定义点，继承
+// 当我们想在点结构中加入其他信息，比如一个字段id，该怎么办？你可能会使用继承，
+// 但这样你就不能用相关算法了，因为想要使用相关算法，类必须要满足对应的concept。
+// 那如何是好？你应该将自定义类，用BOOST_GEOMETRY_REGISTER*宏，
+// 注册到boost的native geometry type中。啥？来个例子你就明白了。
+struct MyPoint // 自定义的点
+{
+    MyPoint(double x, double y, int id) : m_x(x), m_y(y), m_id(id) {}
+    double m_x;
+    double m_y;
+    int m_id; // 标识id
+};
+
+// 将MyPoint注册到bg的native类型中
+BOOST_GEOMETRY_REGISTER_POINT_2D(MyPoint, double, bg::cs::cartesian, m_x, m_y)
+
+
 int main(int argc, char *argv[])
 {
     // cs / coordinate system  坐标系
@@ -16,10 +33,10 @@ int main(int argc, char *argv[])
 
     { // 求地理距离
         using geo_point = bg::model::point<
-            double, 2, bg::cs::geographic<bg::degree>>; //地理坐标系,单位为度
+            double, 2, bg::cs::geographic<bg::degree>>; // 地理坐标系,单位为度
 
-        geo_point gp_beijing(116.23128, 40.22077);  //北京经纬度坐标
-        geo_point gp_shanghai(121.48941, 31.40527); //上海经纬度坐标
+        geo_point gp_beijing(116.23128, 40.22077);  // 北京经纬度坐标
+        geo_point gp_shanghai(121.48941, 31.40527); // 上海经纬度坐标
         auto dis = bg::distance(gp_beijing, gp_shanghai);
         std::cout << "北京到上海的距离是 " << dis / 1000 << " km" << std::endl;
 
@@ -53,14 +70,14 @@ int main(int argc, char *argv[])
 
     { // 点是否在图形内
         using geo_point = bg::model::point<
-            double, 2, bg::cs::cartesian>;           //笛卡尔坐标系
-        using geo_ring = bg::model::ring<geo_point>; //线段
+            double, 2, bg::cs::cartesian>;           // 笛卡尔坐标系
+        using geo_ring = bg::model::ring<geo_point>; // 线段
 
-        geo_ring ring = {{0, 0}, {0, 2}, {2, 2}, {2, 0}, {0, 0}}; //矩形
-        geo_point gp_in(1, 1);                                    //在图形内
-        geo_point gp_out(3, 0);                                   //不在图形内
-        auto is_in = bg::within(gp_in, ring);                     //是否在图形内部
-        auto is_out = bg::within(gp_out, ring);                   //是否在图形内部
+        geo_ring ring = {{0, 0}, {0, 2}, {2, 2}, {2, 0}, {0, 0}}; // 矩形
+        geo_point gp_in(1, 1);                                    // 在图形内
+        geo_point gp_out(3, 0);                                   // 不在图形内
+        auto is_in = bg::within(gp_in, ring);                     // 是否在图形内部
+        auto is_out = bg::within(gp_out, ring);                   // 是否在图形内部
         std::cout << bg::dsv(gp_in) << " locates inside" << bg::dsv(ring) << " " << is_in << std::endl;
         std::cout << bg::dsv(gp_out) << " locates inside" << bg::dsv(ring) << " " << is_out << std::endl;
 
@@ -70,7 +87,7 @@ int main(int argc, char *argv[])
 
     { // 点乘操作
         using geo_point = bg::model::point<
-            double, 2, bg::cs::cartesian>; //笛卡尔坐标系
+            double, 2, bg::cs::cartesian>; // 笛卡尔坐标系
 
         geo_point gp1(1, 0), gp2(1, 1), gp3(0, 1);
         auto dp12 = bg::dot_product(gp1, gp2);
@@ -84,8 +101,8 @@ int main(int argc, char *argv[])
 
     { // 求外接矩形AABB
         using geo_point = bg::model::point<
-            double, 2, bg::cs::cartesian>;                 //笛卡尔坐标系
-        using geo_line = bg::model::linestring<geo_point>; //线
+            double, 2, bg::cs::cartesian>;                 // 笛卡尔坐标系
+        using geo_line = bg::model::linestring<geo_point>; // 线
         using geo_box = bg::model::box<geo_point>;         // box
 
         geo_line line = {{1, 1}, {2, 0}, {3, 2}};
@@ -97,25 +114,8 @@ int main(int argc, char *argv[])
         // ((1, 1), (2, 0), (3, 2))'s AABB is ((1, 0), (3, 2))
     }
 
-    { // 自定义点，继承
-        // 当我们想在点结构中加入其他信息，比如一个字段id，该怎么办？你可能会使用继承，
-        // 但这样你就不能用相关算法了，因为想要使用相关算法，类必须要满足对应的concept。
-        // 那如何是好？你应该将自定义类，用BOOST_GEOMETRY_REGISTER*宏，
-        // 注册到boost的native geometry type中。啥？来个例子你就明白了。
-        struct MyPoint //自定义的点
-        {
-            MyPoint(double x, double y, int id) : m_x(x), m_y(y), m_id(id) {}
-            double m_x;
-            double m_y;
-            int m_id; //标识id
-        };
-
-        //将MyPoint注册到bg的native类型中
-        BOOST_GEOMETRY_REGISTER_POINT_2D(MyPoint, double, cs::cartesian, m_x, m_y)
-
-        namespace bg = boost::geometry;
-        using geo_point = MyPoint;
-        geo_point gp1(0, 0, 1), gp2(1, 0, 2);
+    {
+        MyPoint gp1(0, 0, 1), gp2(1, 0, 2);
         auto dis = bg::distance(gp1, gp2);
         std::cout << bg::dsv(gp1) << " distant from " << bg::dsv(gp2) << " is " << dis << std::endl;
     }
