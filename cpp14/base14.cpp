75b090a8df4a46666a25a334a61fd22441863830
yulinxx <yulinxx@gmail.com>
2023-07-13 15:09:32 +0800

    修复 c++14 --- 能正确编译通过

--- cpp14/base14.cpp
+++ cpp14/base14.cpp
@@ -26,87 +26,49 @@ int main()
     // lambda表达式可以使用auto形参和返回类型，但普通函数不能使用auto形参。
     // 虽然普通函数不能使用auto形参，但可以使用返回类型推导。
     // 在C++ 11中我们可以使用后置返回类型推导来完成这件事情：
-    auto add11a(int x, int y)->decltype(x + y)
+    auto add11a = [](int x, int y)->decltype(x + y)
     {
         return x + y;
     };
 
     // 而在C++ 14中，我们可以省略decltype，编译器直接由return语句的值推导出返回类型：
-    auto add14b(int x, int y)
+    auto add14b = [](int x, int y)
     {
         return x + y;
     };
 
     // 如果函数实现中含有多个 return 语句，这些表达式必须可以推断为相同的类型
-    auto PI1(bool fract)
+    auto PI1 = [](bool fract)
     {
         if (fract)
             return 3.1416;
         else
-            return 3; // 错误：推导出来的int与之前推导出来的double冲突
+            return 3.0;
+            // return 3; // 错误：推导出来的int与之前推导出来的double冲突
     };
 
     // 对于含有递归调用的函数，递归调用必须在第一个return之后
-    auto Correct1(int i)
-    {
-        if (i == 1)
-            return i; // 返回类型被推断为 int
-        else
-            return Correct1(i - 1) + i; // 正确，可以调用
-    };
+    // auto Correct1 = [](int i)
+    // {
+    //     if (i == 1)
+    //         return i; // 返回类型被推断为 int
+    //     else
+    //         return Correct1(i - 1) + i; // 正确，可以调用
+    // };
 
-    auto Wrong1(int i)
-    {
-        if (i != 1)
-            return Wrong(i - 1) + i; // 不能调用，之前没有 return 语句
-        else
-            return i; // 返回类型被推断为 int
-    };
-
-    // lambda表达式可以使用auto形参和返回类型，但普通函数不能使用auto形参。
-    // 虽然普通函数不能使用auto形参，但可以使用返回类型推导。在C++ 11中我们可以使用后置返回类型推导来完成这件事情：
-    // C++11中使用后置返回类型推导
-    auto add11c(int x, int y)->decltype(x + y)
-    {
-        return x + y;
-    };
-
-    // 而在C++ 14中，我们可以省略decltype，编译器直接由return语句的值推导出返回类型：
-    auto add14d(int x, int y)
-    {
-        return x + y;
-    };
-
-    // 如果函数实现中含有多个 return 语句，这些表达式必须可以推断为相同的类型
-    auto PI2(bool fract)
-    {
-        if (fract)
-            return 3.1416;
-        else
-            return 3; // 错误：推导出来的int与之前推导出来的double冲突
-    };
-
-    // 对于含有递归调用的函数，递归调用必须在第一个return之后
-    auto Correct2(int i)
-    {
-        if (i == 1)
-            return i; // 返回类型被推断为 int
-        else
-            return Correct(i - 1) + i; // 正确，可以调用
-    };
-
-    auto Wrong2(int i)
+    auto Wrong1 = [](int i)
     {
         if (i != 1)
-            return Wrong(i - 1) + i; // 不能调用，之前没有 return 语句
+            return i;
+            // return Wrong1(i - 1) + i; // 不能调用，之前没有 return 语句
         else
             return i; // 返回类型被推断为 int
     };
-
+    
     // 二进制字面值
     // 二进制字面值可以使用0b或者0B开头来表示
-    int i = 0b0100010001; // 273
-    int i = 0B0100010001; // 273，大写B
+    int iA = 0b0100010001; // 273
+    int iB = 0B0100010001; // 273，大写B
 
     // 数字分位符
     // C++14 引入单引号（’）作为数字分位符号，使得数值型的字面量可以具有更好的可读性
@@ -122,9 +84,13 @@ int main()
     // 若多元组含有多于一个这个类型的对象，将会产生一个编译错误：
 
     std::tuple<std::string, std::string, int> t("foo", "bar", 7);
-    int i = get<int>(t);                 // i == 7
-    int j = get<2>(t);                   // Same as before: j == 7
-    std::string s = get<std::string>(t); // Compiler error due to ambiguity
+    int i = std::get<int>(t);                 // i == 7
+    int j = std::get<2>(t);                   // Same as before: j == 7
+
+    // 多个相同类型的元素存在于元组中，会导致编译错误
+    // std::string s = std::get<std::string>(t); // Compiler error due to ambiguity
+    std::string s = std::get<0>(t); // No ambiguity: s == "foo"
+
 
     // make_unique
     // std::make_unique 可以像 std::make_shared 一样使用，用于产生 std::unique_ptr 对象
